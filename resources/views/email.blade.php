c541bfec0b2a10318b26c4be3f2da53383bc020d
hafizh4607 <hafizhfauzan01@gmail.com>
2024-11-12 21:25:58 +0700

    alert

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table, th, td {
+            padding: 1px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <p>Dear ...</p>
@@ -31,6 +38,7 @@
             @endforeach
             </tbody>
     </table>
+    <br>
     <p>Best regards</p>
 
 </body>
