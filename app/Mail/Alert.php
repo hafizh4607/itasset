33149d7eccfaac1824d9c0b03b7db37b16878f4a
hafizh4607 <hafizhfauzan01@gmail.com>
2024-11-12 21:10:18 +0700

    alert

--- app/Mail/Alert.php
+++ app/Mail/Alert.php
@@ -37,7 +37,7 @@ class Alert extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.email',
+            view: 'email',
             with: [
                 'form' => $this->form
             ]
